e14a81da00af175ab4ac379643b312282cc5d443
kissge <mail@yo.eki.do>
2018-09-16 17:35:32 +0900

    Recurse directory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 extern crate comrak;
 use comrak::{markdown_to_html, ComrakOptions};
+use std::fs::{self, File};
+use std::io::{self, prelude::*};
+use std::path::Path;
 
 fn main() {
-    println!("{}", markdown_to_html("Hello, **世界**!", &ComrakOptions::default()));
+    let src_dir = "/home/yk/monolog/";
+    recurse(Path::new(src_dir))
+        .expect("recurse failed");
+}
+
+fn recurse(dir: &Path) -> io::Result<()> {
+    for entry in fs::read_dir(dir)? {
+        let path = entry?.path();
+        if path.is_dir() {
+            recurse(&path)?;
+        } else {
+            println!("{}", path.to_str().unwrap());
+            let mut f = File::open(path).expect("file not found");
+
+            let mut contents = String::new();
+            f.read_to_string(&mut contents)
+                .expect("file read failed");
+
+            println!("{}", markdown_to_html(&contents, &ComrakOptions::default()));
+        }
+    }
+    Ok(())
 }
