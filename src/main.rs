bced302db244ce72e6429034ab7048593bb49dc1
kissge <mail@yo.eki.do>
2018-09-16 23:32:39 +0900

    Use glob...this seems smarter?

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
 extern crate comrak;
+extern crate glob;
 use comrak::{markdown_to_html, ComrakOptions};
-use std::fs::{self, File};
-use std::io::{self, prelude::*};
-use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
-    let src_dir = "/home/yk/monolog/";
-    recurse(Path::new(src_dir))
-        .expect("recurse failed");
-}
+    let src_root = "/home/yk/monolog";
 
-fn recurse(dir: &Path) -> io::Result<()> {
-    for entry in fs::read_dir(dir)? {
-        let path = entry?.path();
-        if path.is_dir() {
-            recurse(&path)?;
-        } else {
-            println!("{}", path.to_str().unwrap());
-            let mut f = File::open(path).expect("file not found");
+    for entry in glob::glob(&format!("{}/**/*.md", src_root)).expect("File list up failed") {
+        let entry = entry.unwrap();
 
-            let mut contents = String::new();
-            f.read_to_string(&mut contents)
-                .expect("file read failed");
+        println!("{}", entry.display());
 
-            println!("{}", markdown_to_html(&contents, &ComrakOptions::default()));
-        }
+        let mut f = File::open(entry).expect("file open failed");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).expect("file read failed");
+        println!("{}", markdown_to_html(&contents, &ComrakOptions::default()));
     }
-    Ok(())
 }
