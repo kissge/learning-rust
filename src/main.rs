507f3edb7cd848a591d3ed72b3d4f12b8bec2828
kissge <mail@yo.eki.do>
2018-12-25 02:10:41 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate comrak;
 extern crate glob;
 use comrak::{markdown_to_html, ComrakOptions};
-use std::collections::HashMap;
 use std::env;
 use std::fs::{self, File};
 use std::io::prelude::*;
@@ -13,13 +12,19 @@ fn main() {
 
     env::set_current_dir(Path::new(src_root)).expect(&format!("Cannot cd to {}", src_root));
 
-    let mut entries = HashMap::new();
+    let targets = glob::glob("**/*.md")
+        .expect("File list up failed")
+        .map(|src_path| {
+            let src_path = src_path.unwrap().into_os_string().into_string().unwrap();
 
-    for src_path in glob::glob("**/*.md").expect("File list up failed") {
-        let src_path = src_path.unwrap();
-        let src_path = src_path.to_str().unwrap();
-        assert!(src_path.ends_with(".md"));
-        let canonical_path = src_path.get(..src_path.len() - 3).unwrap();
+            src_path.get(..src_path.len() - 3).unwrap().to_string()
+        })
+        .collect::<Vec<_>>();
+
+    println!("{} target files found.", targets.len());
+
+    for canonical_path in &targets {
+        let src_path = &format!("{}.md", canonical_path);
         let dst_path = &format!("{}/{}.html", dst_root, canonical_path);
 
         fs::create_dir_all(Path::new(&dst_path).parent().unwrap())
@@ -34,16 +39,19 @@ fn main() {
         let mut f = File::create(&dst_path).expect("file create failed");
         f.write_all(markdown_to_html(&contents, &ComrakOptions::default()).as_bytes())
             .expect("file write failed");
-
-        let entry_path = str::replace(&dst_path, dst_root, "");
-        entries.insert(canonical_path.to_string(), entry_path);
     }
 
     let mut f = File::create(&format!("{}/index.html", dst_root)).expect("file create failed");
     f.write(b"<ol>\n").unwrap();
-    for (key, entry) in &entries {
-        f.write(&format!("<li><a href=\"{}\">{}</a></li>\n", entry, key).as_bytes())
-            .unwrap();
+    for canonical_path in targets {
+        f.write(
+            &format!(
+                "<li><a href=\"/{}.html\">{}</a></li>\n",
+                canonical_path, canonical_path
+            )
+            .as_bytes(),
+        )
+        .unwrap();
     }
     f.write(b"</ol>\n").unwrap();
 }
